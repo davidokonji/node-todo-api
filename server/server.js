58115de7aa407f2198e0f34659f8bcca243cc56d
Okonji Chukwunonso <davidokonji3@gmail.com>
2018-12-13 00:55:59 +0100

    commit after post and get route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,16 @@ app.post('/todos', (req, res) => {
         res.status(400).send(err);
     })
 });
+
+app.get('/todos',(req,res)=>{
+    Todo.find().then((doc)=>{
+        res.send({doc});
+    },(err)=>{
+        res.status(400).send(err);
+    })
+});
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
+
+module.exports ={app}
