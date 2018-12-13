3d0fb29e86d1025645cbbd86b2cb13ac169ced3c
Okonji Chukwunonso <davidokonji3@gmail.com>
2018-12-13 01:14:39 +0100

    Testing the get route

--- server/server.js
+++ server/server.js
@@ -24,8 +24,8 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos',(req,res)=>{
-    Todo.find().then((doc)=>{
-        res.send({doc});
+    Todo.find().then((todos)=>{
+        res.send({todos});
     },(err)=>{
         res.status(400).send(err);
     })
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,20 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+//Testing for get route
+const todos =[{
+    text: 'first test todo'
+},{
+    text: 'second test todo'
+}];
+//<-- eof testing get route
 //NOTE: the test suite wont run correctly because our test suite assumes the todo collection is empty,so we run a mocha   
 // life cycle function callled beforeEach()
 beforeEach((done)=>{
- Todo.remove({}).then(()=>done());
+//  Todo.remove({}).then(()=>done());  //this is used for the post test check we modify for get
+Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos);
+}).then(()=>done());
 });
 ////////////////////////////////////////////////
 describe('POST/todos',()=>{
@@ -25,7 +35,7 @@ describe('POST/todos',()=>{
                 return done(err);
             }
             
-            Todo.find().then((todos)=> {      // the assumption of it being empty
+            Todo.find({text}).then((todos)=> {      // the assumption of it being empty
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -43,9 +53,20 @@ describe('POST/todos',()=>{
                 return done(err);
             }
             Todo.find().then((todos)=> {      // the assumption of it being empty
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch((e)=>done(e));
+        });
+    });
+});
+describe('GET /todos',()=>{
+    it('should get all todos',(done)=>{
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todos.length).toBe(2);
         })
-    })
+        .end(done);
+    });
 });
